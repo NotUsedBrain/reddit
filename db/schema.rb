ddae53c8f953917ecc313122bc2cdb03747ca59d
NotUsedBrain <notusedbrain@hotmail.com>
2019-10-03 00:07:45 +0100

    removed unrelated code

--- db/schema.rb
+++ db/schema.rb
@@ -51,11 +51,6 @@ ActiveRecord::Schema.define(version: 2019_10_01_182949) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "posts_users", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
